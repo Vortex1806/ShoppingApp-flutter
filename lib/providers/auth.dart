c0244b78c5799131038469108cb50c0f340a84bf
Vortex1806 <shubhvora03@gmail.com>
2022-06-03 00:34:12 +0530

    Animations and Transitions Added

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/models/http_exception.dart';
+import '../models/http_exception.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
